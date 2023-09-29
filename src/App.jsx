552d433478bb1ce9e77cc5cab9fc50d6be1b9162
RealRajnish <rajnishf3@gmail.com>
2023-09-29 20:07:36 +0530

    basic - pages and routes set with their respective components

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,15 @@ import { useEffect } from "react"
 import { fetchDataFromApi } from "./utils/api"
 import { useDispatch, useSelector } from "react-redux"
 import { getApiConfiguration } from "./store/homeSlice"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+
+import Home from "./pages/home/Home"
+import Header from "./components/header/Header"
+import Footer from "./components/footer/Footer"
+import Details from "./pages/details/Details"
+import SearchResult from "./pages/searchResult/SearchResult"
+import Explore from "./pages/explore/Explore"
+import PageNotFound from "./pages/404/PageNotFound"
 
 
 
@@ -22,11 +31,17 @@ function App() {
   },[])
   
   return (
-    <>
-     <div className="App" style={{color:"whitesmoke"}}>hey buddy
-     {url?.total_pages}
-     </div>
-    </>
+    <BrowserRouter>
+    {/* <Header/> */}
+    <Routes>
+      <Route path="/" element={<Home/>} />
+      <Route path="/:mediaType/:id" element={<Details/>}/>
+      <Route path="/search/:query" element={<SearchResult/>}/>
+      <Route path="/explore/:mediaType" element={<Explore/>}/>
+      <Route path="*" element={<PageNotFound/>}/>
+    </Routes>
+    {/* <Footer/> */}
+    </BrowserRouter>
   )
 }
 
