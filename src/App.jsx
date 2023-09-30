f45279cea2d36d302eee8280f31fb06ffbd46c7d
RealRajnish <rajnishf3@gmail.com>
2023-09-30 16:08:13 +0530

    feat - hero section on home page is added

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,50 @@
-import { useEffect } from "react"
-import { fetchDataFromApi } from "./utils/api"
-import { useDispatch, useSelector } from "react-redux"
-import { getApiConfiguration } from "./store/homeSlice"
-import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { useEffect } from "react";
+import { fetchDataFromApi } from "./utils/api";
+import { useDispatch, useSelector } from "react-redux";
+import { getApiConfiguration } from "./store/homeSlice";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from "./pages/home/Home"
-import Header from "./components/header/Header"
-import Footer from "./components/footer/Footer"
-import Details from "./pages/details/Details"
-import SearchResult from "./pages/searchResult/SearchResult"
-import Explore from "./pages/explore/Explore"
-import PageNotFound from "./pages/404/PageNotFound"
-
-
-
-
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+import Footer from "./components/footer/Footer";
+import Details from "./pages/details/Details";
+import SearchResult from "./pages/searchResult/SearchResult";
+import Explore from "./pages/explore/Explore";
+import PageNotFound from "./pages/404/PageNotFound";
 
 function App() {
-  const dispatch = useDispatch()
-  const url=useSelector((state)=>state.home.url)
-  console.log(url)
- const apiTesting=()=>{
-    fetchDataFromApi('/movie/popular').then((res)=>{
-      console.log(res)
-      dispatch(getApiConfiguration(res))
-    })
-  }
-  useEffect(()=>{
-    apiTesting()
-  },[])
-  
+  const dispatch = useDispatch();
+  const url = useSelector((state) => state.home.url);
+  console.log(url);
+  const fetchApiConfig = () => {
+    fetchDataFromApi("/configuration").then((res) => {
+      console.log(res);
+      const url = {
+        backdrop: res.images.secure_base_url + "original",
+        poster: res.images.secure_base_url + "original",
+        profile: res.images.secure_base_url + "original",
+      };
+
+      dispatch(getApiConfiguration(url));
+    });
+  };
+  useEffect(() => {
+    fetchApiConfig();
+  }, []);
+
   return (
     <BrowserRouter>
-    {/* <Header/> */}
-    <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/:mediaType/:id" element={<Details/>}/>
-      <Route path="/search/:query" element={<SearchResult/>}/>
-      <Route path="/explore/:mediaType" element={<Explore/>}/>
-      <Route path="*" element={<PageNotFound/>}/>
-    </Routes>
-    {/* <Footer/> */}
+      {/* <Header/> */}
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/:mediaType/:id" element={<Details />} />
+        <Route path="/search/:query" element={<SearchResult />} />
+        <Route path="/explore/:mediaType" element={<Explore />} />
+        <Route path="*" element={<PageNotFound />} />
+      </Routes>
+      {/* <Footer/> */}
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
