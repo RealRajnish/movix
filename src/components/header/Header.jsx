397a2807615720e4699cd6790d16d18a0de9c435
RealRajnish <rajnishf3@gmail.com>
2023-10-01 19:58:13 +0530

    feat - Header basic skeleton added with the styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,36 @@
+import { useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { HiOutlineSearch } from "react-icons/hi";
 
-import "./style.scss"
+import ContentWrapper from "../contentWrapper/ContentWrapper";
+
+import logo from "../../assets/movix-logo.svg";
+import "./style.scss";
 
 const Header = () => {
+  const [show, setShow] = useState("top");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [query, setQuery] = useState("");
+  const [showSearch, setShowSearch] = useState("");
+  const navigate = useNavigate();
+  const location = useLocation();
   return (
-    <div>Header</div>
-  )
-}
+    <header className="header">
+      <ContentWrapper>
+        <div className="logo">
+          <img src={logo} alt="" />
+        </div>
+        <ul className="menuItems">
+          <li className="menuItem">Movies</li>
+          <li className="menuItem">TV Shows</li>
+          <li className="menuItem">
+            <HiOutlineSearch />
+          </li>
+        </ul>
+      </ContentWrapper>
+    </header>
+  );
+};
 
-export default Header
+export default Header;
