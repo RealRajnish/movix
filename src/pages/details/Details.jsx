12496f860d12735ac0c90336113fe7a6425731db
RealRajnish <rajnishf3@gmail.com>
2023-10-06 11:53:44 +0530

    feat - Official video section added in details page

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom";
 import "./style.scss";
 import useFetch from "../../hooks/useFetch";
 import DetailsBanner from "./detailsBanner/DetailsBanner";
+import Cast from "./cast/Cast";
+import VideosSection from "./videosSection/VideosSection";
 const Details = () => {
   const { mediaType, id } = useParams();
   const { data, loading } = useFetch(`/${mediaType}/${id}/videos`);
@@ -11,6 +13,8 @@ const Details = () => {
   return (
     <div>
       <DetailsBanner video={data?.results?.[0]} crew={credits?.crew} />
+      <Cast data={credits?.cast} loading={creditsLoading} />
+      <VideosSection data={data} loading={loading} />
     </div>
   );
 };
